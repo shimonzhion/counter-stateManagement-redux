b59436e3c0ca29a5e0d598805241328fbd55ab25
shimonzhion <brys94204@gmail.com>
2022-09-05 02:13:40 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { createStore } from "redux";
+import { createStore,compose } from "redux";
 import { counter } from "./store/reducers/counter-reducer";
 import { Provider } from "react-redux";
 
-const store = createStore(counter);
+
+const enhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+ window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+ 
+
+const store = createStore(counter,enhancers);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
